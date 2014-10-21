d80287327519eefd5764c02d6f9ba1a6a022728c
Mick Koch <mick@kochm.co>
2014-10-21 18:46:49 +0000

    Ensure the initial workaround carriage return is flushed
    before returning from the constructor

--- atsci.py
+++ atsci.py
@@ -4,6 +4,7 @@ class AtSciSensor(serial.Serial):
     def __init__(self, file):
         super().__init__(file, 38400)
         self.write("")
+        self.flush()
 
     def write(self, cmd):
         super().write("{}\r".format(cmd).encode("ascii"))
