f8c6a9753f44b8a3bef41004cde484f3a9e457eb
Mick Koch <mick@kochm.co>
2014-10-22 16:05:13 +0000

    Add a comment about the 0xFF byte

--- atsci.py
+++ atsci.py
@@ -3,6 +3,10 @@ import serial
 class AtSciSerial(serial.Serial):
     def __init__(self, tty):
         super().__init__(tty, 38400)
+
+        # The Pi's UART board sends an 0xFF byte every time the tty is opened,
+        # so send a carriage to flush any sensors that are listening for a
+        # command.
         self.write("")
         self.flush()
 
