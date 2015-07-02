5a43d1003468b69dcdc4bf4cc516b1621e5482bc
Mick Koch <mick@kochm.co>
2015-07-02 13:21:56 -0400

    Move 0002 to 0003 and add new 0002

--- dtnsense/records.py
+++ dtnsense/records.py
@@ -12,9 +12,8 @@ class RecordFormat0001(Record):
     __slots__ = ["cfg", "date", "temp", "ph", "cond", "tz"]
 
     def __init__(self, cfg, date, temp, ph, cond, tz="+0000"):
-        assert -999.995 < temp < 999.995
-        assert 0 <= ph < 99.9995
-        assert 0 <= cond < 999999.995
+        self.verify_temp(temp)
+        self.verify_readings(ph, cond)
 
         self.cfg = cfg
         self.date = date
@@ -22,6 +21,13 @@ class RecordFormat0001(Record):
         self.ph = ph
         self.cond = cond
         self.tz = tz
+
+    def verify_temp(self, temp):
+        assert -999.995 < temp < 999.995
+
+    def verify_readings(self, ph, cond):
+        assert 0 <= ph < 99.9995
+        assert 0 <= cond < 999999.995
 
     def format_signed(self, fmt, num):
         # This has to be done this way because python's format strings include
@@ -35,11 +41,32 @@ class RecordFormat0001(Record):
         yield str(self.cfg.fmt)
         yield self.date.strftime("%Y-%m-%d %H:%M:%S")
         yield self.tz
-        yield self.format_signed("{:6.2f}", self.temp)
+
+        for field in self.reading_fields():
+            yield field
+
+    def temp_field(self):
+        return self.format_signed("{:6.2f}", self.temp)
+
+    def reading_fields(self):
+        yield self.temp_field()
         yield "{:6.3f}".format(self.ph)
         yield "{:9.2f}".format(self.cond)
 
 class RecordFormat0002(RecordFormat0001):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def verify_readings(self, ph, cond):
+        assert -99.9995 < ph < 99.9995
+        assert -999999.995 < cond < 999999.995
+
+    def reading_fields(self):
+        yield self.temp_field()
+        yield self.format_signed("{:6.3f}", self.ph)
+        yield self.format_signed("{:9.2f}", self.cond)
+
+class RecordFormat0003(RecordFormat0002):
     def __init__(self, voltage, *args):
         super().__init__(*args)
 
